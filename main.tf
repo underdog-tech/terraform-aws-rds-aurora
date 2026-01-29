47e7a4c370147aa220ab14aa704ffb4e1ff6bf3f
Jason Ramey <jason.ramey@pinwheelapi.com>
2026-01-29 10:18:56 -0500

    INFRA-1309 deletion_protection_enabled = true for cloudwatch log groups

--- main.tf
+++ main.tf
@@ -407,9 +407,10 @@ resource "aws_db_parameter_group" "this" {
 resource "aws_cloudwatch_log_group" "this" {
   for_each = toset([for log in var.enabled_cloudwatch_logs_exports : log if local.create_meta && var.create_cloudwatch_log_group && !var.cluster_use_name_prefix])
 
-  name              = "/aws/rds/cluster/${var.name}/${each.value}"
-  retention_in_days = var.cloudwatch_log_group_retention_in_days
-  kms_key_id        = var.cloudwatch_log_group_kms_key_id
+  name                        = "/aws/rds/cluster/${var.name}/${each.value}"
+  deletion_protection_enabled = true // prevent accidental deletion of the log group
+  retention_in_days           = var.cloudwatch_log_group_retention_in_days
+  kms_key_id                  = var.cloudwatch_log_group_kms_key_id
 
   tags = var.tags
 }
